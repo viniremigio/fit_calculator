a5c2124a8dd86b5d51bb1324e2ff6e6400f85ab2
Tiago Vinicius <titiovini85@gmail.com>
2020-03-06 13:25:47 -0300

    formatting output

--- tmb.py
+++ tmb.py
@@ -16,16 +16,18 @@ if objetivo == 'lose':
 elif objetivo == 'gain':
     total_cal_goal = total_cal_day + 400
 
-print("Weight={}kg, Height={}cm, Age={}, Goal={}".format(peso, altura, idade, objetivo))
-print("TMB = {}, TotalCalPerDay={}, TotalMaintenance_{} = {}".format(tmb, total_cal_day, objetivo, total_cal_goal))
+print("\n\n=== Input ===")
+print("Weight = {}kg\nHeight = {}cm\nAge = {}\nGoal = {}\n".format(peso, altura, idade, objetivo))
 
+print("\n=== Output ===")
+print("TMB = {} kcal\nTotalCalPerDay = {} kcal\nTotalMaintenance_{} = {} kcal\n"
+      .format(tmb, total_cal_day, objetivo, total_cal_goal))
 
 proteina_kcal = 2.5 * peso * 4
 gordura_kcal = round(0.8 * peso * 9, 1)
 carbo_kcal = (total_cal_goal - (proteina_kcal+gordura_kcal))
 
-print("Macros: protein_kcal = {}, fat_kcal = {}, carbo_kcal = {}".format(proteina_kcal, gordura_kcal, carbo_kcal))
-
-print("Macros: protein_g = {}, fat_g = {}, carbo_g = {}".format(proteina_kcal/4, gordura_kcal/9, carbo_kcal/4))
-
-
+print("\n=== Macros ===")
+print("Protein = {} kcal\nFat = {} \nCarbo = {} kcal\n".format(proteina_kcal, gordura_kcal, carbo_kcal))
+print("Protein = {} g\nFat = {} g\nCarbo = {} g\n\n"
+      .format(round(proteina_kcal/4, 0), round(gordura_kcal/9, 0), round(carbo_kcal/4, 0)))
