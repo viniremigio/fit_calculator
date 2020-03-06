b11bc1312d3359cbf4bdcd8b044fd47626d01af6
Tiago Vinicius <titiovini85@gmail.com>
2020-03-06 13:07:20 -0300

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fit Calculator
+
+A gist of how you can calculate the amount of calories per day you should eat in order to gain or lose weight!
+
+## How to run
+
+```
+python tmb.py <weight> <height> <age> <goal: lose|gain>
+```
--- tmb.py
+++ tmb.py
@@ -5,17 +5,18 @@ altura = float(sys.argv[2])
 idade = float(sys.argv[3])
 objetivo = sys.argv[4]
 
-nivel_atividade = 1.55 # moderado
+# Moderate
+nivel_atividade = 1.55
 
-tmb = round((10*peso) + (6.25*altura) - (5*idade) + 5,0)
-total_cal_day = round(tmb*nivel_atividade,0)
+tmb = round((10 * peso) + (6.25 * altura) - (5 * idade) + 5, 0)
+total_cal_day = round(tmb*nivel_atividade, 0)
 
-if objetivo == 'perda':
+if objetivo == 'lose':
     total_cal_goal = total_cal_day * 0.8
-elif objetivo == 'ganho':
+elif objetivo == 'gain':
     total_cal_goal = total_cal_day + 400
 
-print("Peso={}, Altura={}, Idade={}, Objetivo = {}".format(peso, altura, idade, objetivo))
+print("Weight={}kg, Height={}cm, Age={}, Goal={}".format(peso, altura, idade, objetivo))
 print("TMB = {}, TotalCalPerDay={}, TotalMaintenance_{} = {}".format(tmb, total_cal_day, objetivo, total_cal_goal))
 
 
@@ -23,8 +24,8 @@ proteina_kcal = 2.5 * peso * 4
 gordura_kcal = round(0.8 * peso * 9, 1)
 carbo_kcal = (total_cal_goal - (proteina_kcal+gordura_kcal))
 
-print("Macros: proteina_kcal = {}, gordura_kcal = {}, carbo_kcal = {}".format(proteina_kcal, gordura_kcal, carbo_kcal))
+print("Macros: protein_kcal = {}, fat_kcal = {}, carbo_kcal = {}".format(proteina_kcal, gordura_kcal, carbo_kcal))
 
-print("Macros: proteina_g = {}, gordura_g = {}, carbo_g = {}".format(proteina_kcal/4, gordura_kcal/9, carbo_kcal/4))
+print("Macros: protein_g = {}, fat_g = {}, carbo_g = {}".format(proteina_kcal/4, gordura_kcal/9, carbo_kcal/4))
 
 
